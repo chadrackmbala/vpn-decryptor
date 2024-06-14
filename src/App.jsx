b61e2f9f44d2ead2482445e229de1f3a215992e9
chadrackmbala <chadrackmbala20@gmail.com>
2024-06-14 13:26:16 +0200

    set login page

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import './App.css'
 import Button from '@mui/material/Button';
 import Home from './components/home';
 import Login from './components/login';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="bg-[#d1c6c6] h-screen flex justify-center items-center">
+    <div className="bg-white h-screen w-screen flex justify-center items-center">
 
       {/* <h1>VPN Decryptor</h1>
       <Button variant="contained">Connexion</Button>
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,12 +1,31 @@
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LockPersonIcon from '@mui/icons-material/LockPerson';
+import TextField from '@mui/material/TextField';
+import { FormControl, InputLabel, OutlinedInput, IconButton, InputAdornment, } from '@mui/material';
+import Email from '@mui/icons-material/Email';
+
 export default function Login() {
     return (
-        <div>
-            <h1 className="text-3xl font-bold underline">
+        <div className="flex flex-col gap-4 justify-cneter items-cneter">
+            {/* <h1 className="text-3xl font-bold underline">
                 VPN Decryptor
-            </h1>
-            <div className="w-80 h-80 bg-[#f3f4f6]">
-
-            </div>
+            </h1> */}
+            <AccountCircleIcon className='w-[300px] h-[400px] text-[#319484]' />
+            <TextField
+              className='w-60'
+              id="outlined-password-input"
+              label="Adresse E-mail"
+              type="email"
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <Email className='text-[#319484]' />
+                  </InputAdornment>
+                ),
+              }}
+              name='email'
+              
+            />
         </div>
     )
 }
